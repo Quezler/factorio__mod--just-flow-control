2f4a1f2c161d7200d5c14cebba214791e322af40
Patrick 'Quezler' Mounier <Quezler@me.com>
2020-06-22 18:15:54 +0200

    Add files via upload

---
+++ data-final-fixes.lua
@@ -0,0 +1,7 @@
+data.raw["recipe"]["pipe-elbow"   ].hidden = true;
+data.raw["recipe"]["pipe-junction"].hidden = true;
+data.raw["recipe"]["pipe-straight"].hidden = true;
+
+data.raw["item"]["pipe-elbow"   ].hidden = true;
+data.raw["item"]["pipe-junction"].hidden = true;
+data.raw["item"]["pipe-straight"].hidden = true;
